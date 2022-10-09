985b9eb4d0578b943c2c299d2379bdefb4f81e71
amanjain2601 <amanjain2601@gmail.com>
2022-10-09 20:19:27 +0530

    changes made in signup.js file

--- amazon-client/src/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-axios.defaults.withCredentials = true;
-
-const instance = axios.create({
-  baseURL: 'https://amazon-backend-mern.herokuapp.com',
-});
-
-export default instance;
